7799f09cf00b706562ea65c45a7dbcad4d47b145
shahriaranwar <shahriaranwar@augmedix.com>
2017-06-15 16:09:04 +0600

    Adds delete email method

--- gmail_delete_mesage_by_label.py
+++ gmail_delete_mesage_by_label.py
@@ -29,3 +29,36 @@ passwordElem.send_keys(Keys.ENTER)
 time.sleep(5)
 
 driver.get('https://mail.google.com/mail/u/0/#label/' + labelName)
+
+def findUnreadCount(label):
+    if label is None:
+        return 0
+    else:
+        arr = label.split(' ')
+        return int(str(arr[1]))
+
+def findLabel(driver, labelName):
+    labelElem = driver.find_element_by_xpath('//a[@href="https://mail.google.com/mail/u/0/#label/' + labelName + '"]')
+    return labelElem.get_attribute('aria-label')
+
+def deleteEmails(driver):
+    unread = findUnreadCount(findLabel(labelName))
+    while (unread > 0):
+        checkElems=driver.find_elements_by_xpath('//*[@aria-checked="false"]')
+        allCheck=checkElems[9]
+        allCheck.click()
+        print('Selecting all email on the screen')
+        time.sleep(2)
+        delElem=driver.find_element_by_xpath('//*[@aria-label="Delete"]')
+        delElem.click()
+        print('Deleting selected emails on the screen')
+        time.sleep(5)
+        unread = findUnreadCount(findLabel(labelName))
+        if label is None:
+            unread=0
+        else:
+            arr=label.split(' ')
+            unread=int(str(arr[1]))
+        print('Remaining unread: ' + str(unread))
+
+

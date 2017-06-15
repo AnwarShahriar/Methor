0dfb5efd5165388acc01745555ee8a8176ac36cf
shahriaranwar <shahriaranwar@augmedix.com>
2017-06-15 15:55:33 +0600

    Log in and redirect to proper label page

--- gmail_delete_mesage_by_label.py
+++ gmail_delete_mesage_by_label.py
@@ -1,13 +1,31 @@
 import os
 import getpass
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 username = raw_input('Enter Gmail email: ')
 password = getpass.getpass('Enter Gmail password: ')
-label = raw_input('Enter the Label you want to delete: ')
+labelName = raw_input('Enter the Label you want to delete: ')
 
 chromedriver = './chromedriver'
 os.environ['webdriver.chrome.driver'] = chromedriver
 
 driver = webdriver.Chrome(chromedriver)
 driver.get('https://mail.google.com')
+
+time.sleep(5)
+
+usernameElem = driver.find_element_by_id('identifierId')
+usernameElem.send_keys(username)
+usernameElem.send_keys(Keys.ENTER)
+
+time.sleep(3)
+
+passwordElem = driver.find_element_by_name('password')
+passwordElem.send_keys(password)
+passwordElem.send_keys(Keys.ENTER)
+
+time.sleep(5)
+
+driver.get('https://mail.google.com/mail/u/0/#label/' + labelName)

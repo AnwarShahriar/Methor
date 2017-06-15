e8022e138daa993f2ffc6af8df041e10c373d7cd
shahriaranwar <shahriaranwar@augmedix.com>
2017-06-15 15:45:14 +0600

    Take username, password and label input to delete email

--- gmail_delete_mesage_by_label.py
+++ gmail_delete_mesage_by_label.py
@@ -1,5 +1,10 @@
 import os
+import getpass
 from selenium import webdriver
+
+username = raw_input('Enter Gmail email: ')
+password = getpass.getpass('Enter Gmail password: ')
+label = raw_input('Enter the Label you want to delete: ')
 
 chromedriver = './chromedriver'
 os.environ['webdriver.chrome.driver'] = chromedriver
